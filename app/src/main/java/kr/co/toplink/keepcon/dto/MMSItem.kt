1e100c81ca30e093320f9fbe62833df00e5f5110
김선생 <0176262502@nate.com>
2023-07-17 16:01:25 +0900

    회사

--- app/src/main/java/kr/co/toplink/keepcon/dto/MMSItem.kt
+++ app/src/main/java/kr/co/toplink/keepcon/dto/MMSItem.kt
@@ -3,6 +3,7 @@ package kr.co.toplink.keepcon.dto
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(
     tableName = "mms_item"
@@ -12,4 +13,21 @@ data class MMSItem(
     val phoneNumber : String,
     @ColumnInfo(name = "beforeDate")
     val beforeDate : String
+)
+
+@Entity(
+    tableName = "gifticon_item"
+)
+data class GifticonItem(
+    @PrimaryKey @ColumnInfo(name = "barcodeNum")
+    val barcodeNum :	    String,
+    val barcodePos :	    String?,
+    val barcode_filepath :	String?,
+    val brand:			    String?,
+    val due	:               Date?,
+    val price	:		    Int = 0,
+    val memo	:		    String?,
+    val productName:		String?,
+    val productPost:		String?,
+    val state	:		    Int = 0
 )
